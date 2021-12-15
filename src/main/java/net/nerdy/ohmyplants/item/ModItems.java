0dcd2307cb4a4d40ff0025c21088f2c95f34b037
Nerdy_Dragon <zhero240@gmail.com>
2021-12-15 18:31:20 +0200

    Adds first Item: Apple Tree

--- src/main/java/net/nerdy/ohmyplants/item/ModItems.java
+++ src/main/java/net/nerdy/ohmyplants/item/ModItems.java
@@ -1,14 +1,20 @@
 package net.nerdy.ohmyplants.item;
 
+import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 import net.nerdy.ohmyplants.OhMyPlants;
 
 public class ModItems {
     public static final DeferredRegister<Item> ITEMS =
             DeferredRegister.create(ForgeRegistries.ITEMS, OhMyPlants.MOD_ID);
+
+    public static final RegistryObject<Item> APPLE_TREE = ITEMS.register("apple_tree",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
